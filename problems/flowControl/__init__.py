e4802e86d1abc6bb70b744096b2b2498438292dd
Hargreaves <sgchargr@liverpool.ac.uk>
2020-10-19 21:14:38 +0100

    Made these good for check0

--- problems/flowControl/__init__.py
+++ problems/flowControl/__init__.py
@@ -14,29 +14,29 @@ def ly_compiles():
 @check50.check(ly_compiles)
 def test1980():
     """Ensures LeapYear is returning the specified value for 1980"""
-    check50_java.run("LeapYear").stdin(1980).stdout("true\n").exit()
+    check50_java.run("LeapYear").stdin("1980").stdout("true\n").exit()
 
 @check50.check(ly_compiles)
 def test2000():
     """Ensures LeapYear is returning the specified value for 2000"""
-    check50_java.run("LeapYear").stdin(2000).stdout("true\n").exit()
+    check50_java.run("LeapYear").stdin("2000").stdout("true\n").exit()
 
 @check50.check(ly_compiles)
 def test2016():
     """Ensures LeapYear is returning the specified value for 2016"""
-    check50_java.run("LeapYear").stdin(2016).stdout("true\n").exit()
+    check50_java.run("LeapYear").stdin("2016").stdout("true\n").exit()
 
 @check50.check(ly_compiles)
 def test1900():
     """Ensures LeapYear is returning the specified value for 1980"""
-    check50_java.run("LeapYear").stdin(1900).stdout("false\n").exit()
+    check50_java.run("LeapYear").stdin("1900").stdout("false\n").exit()
 
 @check50.check(ly_compiles)
 def test2001():
     """Ensures LeapYear is returning the specified value for 2001"""
-    check50_java.run("LeapYear").stdin(2001).stdout("false\n").exit()
+    check50_java.run("LeapYear").stdin("2001").stdout("false\n").exit()
 
 @check50.check(ly_compiles)
 def test2018():
     """Ensures LeapYear is returning the specified value for 2018"""
-    check50_java.run("LeapYear").stdin(2018).stdout("false\n").exit()
+    check50_java.run("LeapYear").stdin("2018").stdout("false\n").exit()
