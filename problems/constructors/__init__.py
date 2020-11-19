a5c0575615f3690f3313f8d7de2debadd00be94d
SurgeArrester <cameron.h@rgreaves.me.uk>
2020-11-19 13:31:17 +0000

    Updated the right file this time...

--- problems/constructors/__init__.py
+++ problems/constructors/__init__.py
@@ -1,6 +1,6 @@
 import check50
 import check50_java
-
+import check50_junit
 
 @check50.check()
 def student_exists():
@@ -15,7 +15,7 @@ def student_compiles():
 @check50.check(student_compiles)
 def student_constructor_blank():
     """Student constructor with no parameters"""
-    check50_java.junit5.run_and_interpret_test(
+    check50_junit.run_and_interpret_test(
         classpaths=['tests/'],
         args=['--select-method', 'StudentTest#testBlankConstructor'])
 
@@ -23,7 +23,7 @@ def student_constructor_blank():
 @check50.check(student_compiles)
 def student_constructor_full():
     """Student constructor with all parameters"""
-    check50_java.junit5.run_and_interpret_test(
+    check50_junit.run_and_interpret_test(
         classpaths=['tests/'],
         args=['--select-method', 'StudentTest#testFullConstructor'])
 
@@ -31,7 +31,7 @@ def student_constructor_full():
 @check50.check(student_compiles)
 def student_constructor_partial_1():
     """Student constructor with some parameters"""
-    check50_java.junit5.run_and_interpret_test(
+    check50_junit.run_and_interpret_test(
         classpaths=['tests/'],
         args=['--select-method', 'StudentTest#testPartialConstructor1'])
 
@@ -39,7 +39,7 @@ def student_constructor_partial_1():
 @check50.check(student_compiles)
 def student_constructor_partial_2():
     """Student constructor with some parameters in a different format"""
-    check50_java.junit5.run_and_interpret_test(
+    check50_junit.run_and_interpret_test(
         classpaths=['tests/'],
         args=['--select-method', 'StudentTest#testPartialConstructor2'])
 
@@ -47,7 +47,7 @@ def student_constructor_partial_2():
 @check50.check(student_compiles)
 def student_constructor_partial_3():
     """Student constructor with some parameters in a different format with different details"""
-    check50_java.junit5.run_and_interpret_test(
+    check50_junit.run_and_interpret_test(
         classpaths=['tests/'],
         args=['--select-method', 'StudentTest#testPartialConstructor3'])
 
