99c091fdcf093f4cc0eb3484db3a32d7f65e5677
SurgeArrester <cameron.h@rgreaves.me.uk>
2021-02-03 20:14:31 +0000

    Updated tests

--- problems/newton/__init__.py
+++ problems/newton/__init__.py
@@ -39,13 +39,13 @@ def root_9_001():
 
 @check50.check(newt_compiles)
 def root_25_0001():
-    """Ensures Newton is returning the specified value for 25 0.1"""
-    check50_java.run("Newton 25 0.1 0.0001").stdout("11\n5.0").exit()
+    """Ensures Newton is returning the specified value for 25 0.1 1"""
+    check50_java.run("Newton 25 0.1 1").stdout("8\n5.060078220517644").exit()
 
 @check50.check(newt_compiles)
 def root_inf_00001():
-    """Ensures Newton is returning the correct (but incorrect) value for 25 0"""
-    check50_java.run("Newton 25 0 0.00001").stdout("3\nInfinity").exit()
+    """Ensures Newton is returning the correct (but incorrect) value for 25 0 0.1"""
+    check50_java.run("Newton 25 0 0.00001").stdout("9\n5.000356653832537").exit()
 
 @check50.check(newt_compiles)
 def too_few_parameters():
