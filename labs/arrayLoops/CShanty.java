f02c7f98dfa30b01eab8692173757073c1a53dc2
SurgeArrester <cameron.h@rgreaves.me.uk>
2021-02-12 10:42:01 +0000

    Updated started code

--- labs/arrayLoops/CShanty.java
+++ labs/arrayLoops/CShanty.java
@@ -1,12 +1,13 @@
 public class CShanty {
     public static void main(String[] args) {
-        int n = ?
+        int n = Comp122.getInt("How many rounds we doin' mateys?");
 
-        for(? ; ? ; ?) {
-            System.out.println("Oh I like to code and OOP sets me free!");
-            System.out.println("For reliable code, Java is for me");
+        for(int i = 0; i < n ; i++ ) {
+            System.out.println("Soon may the compiler come,");
+            System.out.println("To bring class files and exceptions");
         }
 
-        System.out.println("But, my first love is the C!");
+        System.out.println("High level languages earn my rum");
+        System.out.println("But my first love is the C!");
     }
 }
