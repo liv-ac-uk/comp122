58bba8c0b35dde59b325e7d3a579cc4cb937bfff
SurgeArrester <cameron.h@rgreaves.me.uk>
2021-02-22 16:15:58 +0000

    Fixed typo in respinse string

--- labs/arrayLoops/CShanty.java
+++ labs/arrayLoops/CShanty.java
@@ -2,7 +2,7 @@ public class CShanty {
     public static void main(String[] args) {
         int n = Comp122.getInt("How many rounds we doin' mateys?");
 
-        for(int i = 0; i < n ; i++ ) {
+        for(? ; ? ; ?) {
             System.out.println("Soon may the compiler come,");
             System.out.println("To bring class files and exceptions");
         }
