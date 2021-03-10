2ffe6c5ee931cd7d76c6cfdb80259273c3922b8f
SurgeArrester <cameron.h@rgreaves.me.uk>
2021-03-10 23:06:28 +0000

    Merged abstract for testing

---
+++ labs/abstract/Student.java
@@ -0,0 +1,12 @@
+public class Student extends Person {
+      private Int grade;
+
+      public void setGrade(Int new_grade) {
+          grade = new_grade;
+      }
+
+      public Int getGrade() {
+          return grade;
+      }
+  }
+  
