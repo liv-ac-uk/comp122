110e0e16a5a30ab284b826e828094d252bd5fb6f
Totzke, Patrick <totzke@liverpool.ac.uk>
2021-03-28 14:42:55 +0100

    vigenere: repeat duplicate check and fix caesar usage print string

--- problems/vigenere/__init__.py
+++ problems/vigenere/__init__.py
@@ -343,7 +343,7 @@ def caesar_check_one_arg():
 @check50.check(caesar_main_exists)
 def caesar_check_first_arg():
     """See if we get the right error message when Caesar is run with an incorrect first argument"""
-    check50_java.run("Caesar dec 12 lala").stdout("The first parameter must be \"encrypt\" or \"decrypt\"!\nUsage: java Caesar encrypt key \"cipher text\"\n")
+    check50_java.run("Caesar dec 12 lala").stdout("The first parameter must be \"encrypt\" or \"decrypt\"!\nUsage: java Caesar encrypt n \"cipher text\"\n")
 
 
 # # # Vigenere ###############################
@@ -442,11 +442,6 @@ def vigenere_main_test_02():
     """java Vigenere decrypt COMPONETWOTWO \"hiz thr big\" works as expected"""
     check50_java.run("Vigenere decrypt COMPONETWOTWO \"hiz thr big\"").stdout("fun fun fun\n")
 
-# TODO This is just a repeat of the test below, make better
-@check50.check(vigenere_main_returns_successfully)
-def vigenere_main_test_03():
-    """java Vigenere enc COMPONETWOTWO \"fun fun fun\" returns correct error message"""
-    check50_java.run("Vigenere enc COMPONETWOTWO \"fun fun fun\"").stdout("The first parameter must be \"encrypt\" or \"decrypt\"!\nUsage: java Vigenere encrypt key \"cipher text\"\n")
 
 
 @check50.check(vigenere_main_exists)
