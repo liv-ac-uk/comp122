265554531420e4ae303fe93da66ff7929f23068c
Totzke, Patrick <totzke@liverpool.ac.uk>
2021-10-12 11:28:56 +0100

    Lab: arrayLoops

---
+++ labs/arrayLoops/Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial {
+    public static void main(Stringp[] args) {
+        int n = ?;
+        ? factorial = ?;
+
+        for(? ; ? ; ?) {
+
+        }
+
+        System.out.println(factorial);
+    }
+}
---
+++ labs/arrayLoops/Largest.java
@@ -0,0 +1,20 @@
+public class Largest {
+    public static void main(String[] args) {
+        int n = ?
+        int[] myArray = ?
+
+        for (;;) {
+            ? = Comp122.getInt("");
+        }
+
+        int largestValue = ?
+
+        for (;;) {
+            if (?) {
+                ?
+            }
+        }
+
+        System.out.println(largestValue);
+    }
+}
